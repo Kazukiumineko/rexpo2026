ee782188108d9d0c14e248dd774361fab8481cca
kazukiumineko <kazuki.1721.umineko@gmail.com>
2025-12-22 12:30:35 +0900

    second

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-red-500">いちろー</div>
+    <div>
+      <div className="text-red-500">いちろー</div>
+      <div className="text-sky-500">じろう</div>
+      <div className="text-green-500">さくら</div>
+    </div>
   );
 }
