662eac2ce0c26897d1bf4a01b8840a11c9e963b6
kazukiumineko <kazuki.1721.umineko@gmail.com>
2026-01-03 17:16:27 +0900

    New

--- app/comments/page.tsx
+++ app/comments/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
     return (
         <main className="w-full min-h-screen bg-black flex items-center justify-center">
             <h1 className="text-white text-2xl font-bold tracking-widest">
-                COMING SOON...
+                COMING SOON!
             </h1>
         </main>
     );
