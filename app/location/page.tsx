c51c74f4dbf6fe2dffbba7a11c335a19d462c95e
kazukiumineko <kazuki.1721.umineko@gmail.com>
2026-01-04 15:30:17 +0900

    kk

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -7,19 +7,30 @@ import Top from "@/components/LocationUI/Top";
 import Info from "@/components/LocationUI/Info";
 
 export default function LocationPage() {
-    // メニューバーを常に表示するため true 固定
     const [showHeader] = useState(true);
 
     return (
-        <main className="relative w-full bg-white min-h-screen">
+        /* 【修正のポイント】
+           1. flex flex-col: 子要素を縦に並べる
+           2. min-h-screen: 最低でも画面いっぱいの高さを確保する
+           3. bg-black: 背景を黒に指定（もし白が良い場合は bg-white）
+        */
+        <main className="flex flex-col min-h-screen w-full bg-black overflow-x-hidden">
+
             {/* ヘッダー */}
             <Header isVisible={showHeader} />
 
-            {/* ロケーションTOPセクション */}
-            <Top />
+            {/* 【重要：コンテンツエリア】
+                flex-grow をつけることで、中身（TopやInfo）が少なくても、
+                このエリアが自動で伸びてフッターを一番下まで押し下げます。
+            */}
+            <div className="flex-grow">
+                {/* ロケーションTOPセクション */}
+                <Top />
+                <Info />
+            </div>
 
-            <Info />
-
+            {/* フッター：これで必ず「コンテンツの直後」かつ「最低でも画面最下部」に配置されます */}
             <FooterBar />
         </main>
     );
