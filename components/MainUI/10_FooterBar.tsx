3d3100fdca3a6e7d04994763b2961c45f61099dc
kazukiumineko <kazuki.1721.umineko@gmail.com>
2026-01-16 14:03:07 +0900

    cryvuinm

--- components/MainUI/10_FooterBar.tsx
+++ components/MainUI/10_FooterBar.tsx
@@ -14,7 +14,7 @@ export default function FooterBar({ variant = "default" }: FooterBarProps) {
         { label: "コンセプト", href: "event-concept" },
         { label: "ステージ", href: "stage-concept" },
         { label: "会場", href: "location" },
-        { label: "協賛企業", href: "support" },
+        { label: "ご支援", href: "support" },
         { label: "開祭にあたって", href: "message" },
         { label: "一般来場の方", href: "entry" },
         { label: "特設アプリ", href: "application" },
