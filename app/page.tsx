6d82f92079cfa58fd1627899ad0d9c00dc3fc6c3
kazukiumineko <kazuki.1721.umineko@gmail.com>
2026-02-12 00:16:43 +0900

    uohgu2

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,50 @@
-import PasswordLock from "@/components/main-ui/password-lock";
-import ClosingMessage from "@/components/secret-ui/closing-message";
+import HomeTopController from "@/components/main-ui/home-top-controller";
+import ScrollTextSection from "@/components/main-ui/scroll-text-section";
+import ConceptSection from "@/components/main-ui/concept-section";
+import StageSection from "@/components/main-ui/stage-section";
+import InformationSection from "@/components/main-ui/information-section";
+import Application from "@/components/main-ui/application";
+import FooterBar from "@/components/main-ui/footer-bar";
+
+import HippySection from "@/components/main-ui/hippy";
+
 
 export default function Home() {
   return (
     <main className="relative w-full">
 
-      <ClosingMessage />
+      {/* クライアントロジック層 */}
+      <HomeTopController>
+        <HippySection />
+      </HomeTopController>
 
+      {/* <div className="relative z-10 -mt-20 md:-mt-32 pb-20 md:pb-32">
+        <HippySection />
+      </div> */}
+
+      <div className="relative z-10">
+        <ConceptSection />
+      </div>
+
+      <div className="relative z-10 w-full h-24 lg:h-32"></div>
+
+      <div className="relative z-10">
+        <StageSection />
+      </div>
+
+      <div className="relative z-10 w-full bg-white text-black">
+        <div className="w-full h-24 lg:h-32"></div>
+        <InformationSection />
+        <div className="w-full h-24 lg:h-48"></div>
+
+        <Application />
+        <div className="w-full h-2 lg:h-38"></div>
+        <div className="relative z-50">
+          <FooterBar />
+        </div>
+      </div>
     </main>
   );
 }
+
+
